b4ac1f4b5efa1e98b92cb58ef26f1514e1416977
agustin notbook <programacionagus2023@gmail.com>
2023-05-31 20:06:33 -0300

    mofificaciones

--- React/portfolio/src/componetes/About.jsx
+++ React/portfolio/src/componetes/About.jsx
@@ -1,7 +1,18 @@
 import agu from "../assets/agu.jpg"
-
-
+import { useState } from "react"
+import autito2 from "../assets/autito2.jpg"
+ 
 const About = () => {
+
+  
+  const [num, setNum] = useState(0);
+
+  const handleclick = () => {
+    // setCurrentimage((prevImage)=>(prevImage+ 1) % images.length);
+    setNum(num+1)
+  }
+
+
   return (
     <>
         <main className="main_about">
@@ -9,7 +20,9 @@ const About = () => {
                 <h1>Mis trabajos</h1>
 
                 <div className="contenedor_about">
-                    
+                    <h2>{num}</h2>
+                    <img src={autito2} alt="imagen cambiante" />
+                    <button onClick={handleclick}>Cambiar imagen</button>
                 </div>
         </main>    
     
